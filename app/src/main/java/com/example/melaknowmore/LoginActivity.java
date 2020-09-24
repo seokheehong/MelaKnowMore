d66cde69e102036b462f30f58df977dba49124ba
Seok Hee Hong <seokheehong1@gmail.com>
2020-09-23 22:59:27 -0500

    PM3 - MultiPages (Login)

--- app/src/main/java/com/example/melaknowmore/LoginActivity.java
+++ app/src/main/java/com/example/melaknowmore/LoginActivity.java
@@ -6,17 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 
-    public void goToLogin(View view) {
-        Intent intent = new Intent(this,LoginActivity.class);
+    public void goToHome(View view) {
+        Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
     }
-
 }
