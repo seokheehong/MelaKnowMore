8e97535739e2098df73119dd591de8233c91ba45
Seok Hee Hong <seokheehong1@gmail.com>
2020-10-02 15:41:53 -0500

    9/30 PM4

--- app/src/main/java/com/example/melaknowmore/MainActivity.java
+++ app/src/main/java/com/example/melaknowmore/MainActivity.java
@@ -1,21 +1,58 @@
 package com.example.melaknowmore;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar myToolbar;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myToolbar = (Toolbar) findViewById(R.id.myToolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_diagnose:
+                return true;
+            case R.id.menu_more_info:
+                Intent intent = new Intent(this, MoreInfoActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.menu_about_us:
+                return true;
+            case R.id.menu_contact:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void goToLogin(View view) {
-        Intent intent = new Intent(this,LoginActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 
---
+++ app/src/main/java/com/example/melaknowmore/MoreInfoActivity.java
@@ -0,0 +1,31 @@
+package com.example.melaknowmore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MoreInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_info);
+    }
+
+    public void openWebsite(View view) {
+        String url = "https://www.cancer.org/cancer/melanoma-skin-cancer.html";
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        else {
+            Log.d("ImplicitIntents", "Can't handle");
+        }
+    }
+}
